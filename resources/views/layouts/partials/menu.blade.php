ed8f3980fea5ba77fe973d7e7597f7b88fe2160d
HenriquePBoscioni <henriquepra10@gmail.com>
2023-08-28 21:11:25 -0300

    INICIO DE PROJETO

---
+++ resources/views/layouts/partials/menu.blade.php
@@ -0,0 +1,19 @@
+<ul>
+    <li>
+        Olá {{ Auth::user()->name }}
+    </li>
+    <li><a href="{{ route('lancamento.index') }}">
+        Lançamentos
+    </a>
+    </li>
+    <li>
+        <a href="{{ route('centro.index') }}">
+            Centro de Custo
+        </a>
+    </li>
+    <li>
+        <a href="{{ route('logout') }}">
+            Sair
+        </a>
+    </li>
+</ul>
