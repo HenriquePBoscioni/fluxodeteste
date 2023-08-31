71e2fe17f729fe53d26574e59f742aa06dc6b3d7
HenriquePBoscioni <henriquepra10@gmail.com>
2023-08-30 22:17:42 -0300

    Atualizaçao: form, index,show

--- resources/views/centro/show.blade.php
+++ resources/views/centro/show.blade.php
@@ -1,8 +1,29 @@
 @extends('layouts.base')
 @section('content')
-<h1>Show</h1>
+    <h1>Show</h1>
+    <table class="table table-striped  table-hover ">
+
+        <thead>
+            <caption>LISTA DE</caption>
+            <tr>
+                <th class="col-2">#</th>
+                <th>ID</th>
+                <th>Descrição</th>
+                <th>Valor</th>
+            </tr>
+        </thead>
+        <tbody class="table-group-divider">
+            @foreach ($centro as $centro)
+            <tr>
+                <td>
+
+                </td>
+            </tr>
+            @endforeach
+
+        </tbody>
+    </table>
 @endsection
 @section('scripts')
-@parent
-
+    @parent
 @endsection
